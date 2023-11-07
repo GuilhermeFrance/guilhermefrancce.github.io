328741a1e8feef7d6977fd73b73188f5bd6a670c
Guilherme France <gart.2017@gmail.com>
2023-11-06 23:46:50 -0300

    primeiro commit

--- js/email.js
+++ js/email.js
@@ -3,18 +3,18 @@ var container = document.querySelector ('.mailme')
 
 btn.addEventListener('click', function(){
 
-  if(container.style.display === 'flex') {
-      container.style.display = 'none';
+  if(container.style.display === 'none') {
+      container.style.display = 'flex';
   }else {
-    container.style.display = 'flex'
+    container.style.display = 'none'
   }
   
 })
 
-var click = "images/arrowup.png"
-var clickou = "images/arrowdown.png"
+var click = "images/arrowdown.png"
+var clickou = "images/arrowup.png"
 function mudaFoto(){
-document.getElementById('arroww').src = clickou;
+document.getElementById('arroww').src = clickou
 let aux = click;
 click = clickou;
 clickou = aux;
